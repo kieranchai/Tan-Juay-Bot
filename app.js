881d8ef186bd2ba4b4ed7b2387e01b144bedb137
Kieran Chai <mayork383@gmail.com>
2022-05-09 15:24:47 +0800

    Lol

--- app.js
+++ app.js
@@ -1,28 +1,52 @@
 import { Telegraf } from 'telegraf'
 import fetch from 'node-fetch'
+import moment from 'moment';
 
 const BOT_TOKEN = '5372911011:AAHk0VeTdplzPNElB6avZhQj1BJcXNFitcY';
 const bot = new Telegraf(BOT_TOKEN)
 let url = 'https://api.opendota.com/api/players/110236540/recentMatches'
+let wordurl = 'https://api.opendota.com/api/players/110236540/wordcloud'
 
 //BOT COMMANDS
-bot.start((ctx) => ctx.reply('Welcome'))
+bot.start((ctx) => ctx.reply('Gentlemen.'))
 
-bot.hears('juay', async (ctx) =>
+bot.hears('u playing?', async (ctx) =>
     fetch(url).then((res) => res.json()).then((data) => {
-        let juayResult;
-        let matchDt = new Date(data[0].start_time * 1000);
+        let juayResult
+        let matchDt = new Date(data[0].start_time * 1000)
+        let timeAgo = moment(matchDt).fromNow()
         if (data[0].radiant_win == true && data[0].player_slot < 128) {
-            juayResult = "won.";
+            juayResult = "won!!!"
         } else if (data[0].radiant_win == true && data[0].player_slot >= 128) {
-            juayResult = "lost.";
+            juayResult = "lost like a niggin noob"
         } else if (data[0].radiant_win == false && data[0].player_slot < 128) {
-            juayResult = "lost.";
+            juayResult = "lost like a niggin noob"
         } else if (data[0].radiant_win == false && data[0].player_slot >= 128) {
-            juayResult = "won.";
+            juayResult = "won!!!"
         };
-        ctx.reply("Juay's last match was on " + matchDt +", and he " + juayResult);
+        ctx.reply("I last played " + timeAgo + " and I " + juayResult)
     })
-);
+)
+
+bot.hears('selfie leh', async (ctx) => {
+    ctx.replyWithPhoto({ source: 'assets/juayselfie.jpg' }, { caption: "yup sphee" })
+})
+
+bot.hears('bulge pls', async (ctx) => {
+    ctx.replyWithPhoto({ source: 'assets/juaydp.jpg' }, { caption: "nice?" })
+
+})
+
+bot.hears('word', async (ctx) => {
+    fetch(wordurl).then((res) => res.json()).then((data) => {
+        let wordCloud = data.my_word_counts
+        let keyNames = Object.keys(wordCloud)
+        let randomProperty = (obj) => {
+            var keys = Object.keys(obj)
+            return obj[keys[keys.length * Math.random() << 0]]
+        }
+        ctx.reply(randomProperty(keyNames))
+    })
+})
 
 bot.launch()
