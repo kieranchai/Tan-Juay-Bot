345c5aa9a121323601c826a2396b6e68498ec39e
Kieran Chai <mayork383@gmail.com>
2022-05-10 14:08:25 +0800

    FINALIDC

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ dotenv.config()
 import { Telegraf } from 'telegraf'
 import fetch from 'node-fetch'
 import moment from 'moment'
-import * as fs from 'fs'
-import axios from 'axios'
 
 const bot = new Telegraf(process.env.TELEGRAM_API_TOKEN)
 let url = 'https://api.opendota.com/api/players/110236540/recentMatches'
@@ -15,39 +13,6 @@ bot.start((ctx) => {
     let userFirstName = ctx.message.from.first_name
     let message = `Hello master ${userFirstName}, I am Tan Juay Hee your humble servant.`
     ctx.reply(message)
-})
-
-bot.on('photo', async (ctx) => {
-    // const model = new TeachableMachine({
-    //     modelUrl: "https://teachablemachine.withgoogle.com/models/VfU4viXn8/"
-    // })
-    const fileId = ctx.update.message.photo[0].file_id;
-    ctx.reply('I have received the image please give me a moment.')
-    const res = await fetch(
-        `https://api.telegram.org/bot${process.env.TELEGRAM_API_TOKEN}/getFile?file_id=${fileId}`
-    )
-    const res2 = await res.json()
-    const filePath = res2.result.file_path
-    const downloadURL = `https://api.telegram.org/file/bot${process.env.TELEGRAM_API_TOKEN}/${filePath}`;
-    const path1 = `images/${fileId}.jpg`
-    ctx.telegram.getFileLink(fileId).then(downloadUrl => {
-        axios({ url: downloadUrl.href, responseType: 'stream' }).then(response => {
-            return new Promise((resolve, reject) => {
-                response.data.pipe(fs.createWriteStream(path1))
-                    .on('finish', () => {
-                        console.log(path1)
-                        // model.classify({
-                        //     imageUrl: path1,
-                        // }).then((predictions) => {
-                        //     console.log(predictions)
-                        // }).catch((e) => {
-                        //     console.log("ERROR", e);
-                        // })
-                    })
-                    .on('error', e => { })
-            });
-        })
-    })
 })
 
 bot.hears('u playing?', async (ctx) =>
@@ -67,6 +32,10 @@ bot.hears('u playing?', async (ctx) =>
         ctx.reply("I last played " + timeAgo + " and I " + juayResult)
     })
 )
+
+bot.hears('bulge pls?', async (ctx) => {
+    ctx.replyWithPhoto({ source: 'assets/juaydp.jpg' }, { caption: "Don't be sad, have a bulge." })
+})
 
 bot.hears('word', async (ctx) => {
     fetch(wordurl).then((res) => res.json()).then((data) => {
